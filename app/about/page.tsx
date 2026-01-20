558da439c6a53ff5f79703684bd60a41c4b3eb07
Bernardo E. Dramos Jr. <dramosb44@gmail.com>
2026-01-20 16:21:09 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,7 @@
+
 "use client"
 
+import { useRef, useEffect } from "react"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 import { useLanguage } from "@/app/providers"
